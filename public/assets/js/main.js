77bd80385be69b7379909d5ec12b804a55d1a753
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2017-06-19 21:07:28 -0600

    three boxes added mouseover jquery function

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
   console.log('ready');
-
+  $(".main_box").mouseenter((event) => {
+    let div = event.target;
+    let classNames = $(div).attr('class');
+    if (classNames !== 'box_text') {
+      $(event.target).attr('style', 'height: 200px; padding-top: 0.75rem; align-items: flex-start;')
+    }
+  })
+  // $(".main_box").mouseleave((event) => {
+  //   $(event.target).attr('style', 'height: 250px')
+  // })
 })
