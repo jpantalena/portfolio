83fc700aa53698c847ac851d3cbd3cf791e8da4e
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2017-06-19 22:35:57 -0600

    contact links added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,12 +2,11 @@ $(document).ready(() => {
   console.log('ready');
   $(".main_box").mouseenter((event) => {
     let div = event.target;
-    let classNames = $(div).attr('class');
-    if (classNames !== 'box_text') {
-      $(event.target).attr('style', 'height: 200px; padding-top: 0.75rem; align-items: flex-start;')
+    if ($(div).hasClass('main_box')) {
+      $(div).attr('style', 'height: 200px;')
+      let children = $(div).children();
+      let hidden = children[1];
+      $(hidden).attr('style', 'display: flex')
     }
   })
-  // $(".main_box").mouseleave((event) => {
-  //   $(event.target).attr('style', 'height: 250px')
-  // })
 })
