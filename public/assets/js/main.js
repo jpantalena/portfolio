9e80959702f4f7cdf3d84208fb39402c5ad529bf
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2017-06-27 13:52:49 -0600

    updated projects page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,20 +1,19 @@
 $(document).ready(() => {
   console.log('ready');
-  // $(".main_box").mouseenter((event) => {
-  //   let div = event.target;
-  //   if ($(div).hasClass('main_box')) {
-  //     $(div).attr('style', 'height: 200px;')
-  //     let children = $(div).children();
-  //     let hidden = children[1];
-  //     $(hidden).attr('style', 'display: flex')
-  //   }
-  // })
+
   $(".box_text").click(function() {
     let box = $(this).parents()[0];
     let hidden = $(this).next()[0];
-    console.log(hidden);
     $(box).attr('style', 'height: 200px');
     $(hidden).attr('style', 'visibility: visible; opacity: 1;');
-    // $(this).attr('style', 'color:red')
   })
+
+  $(".project_img").click(function() {
+    let img = $(this);
+    let project_text = $(img).next()[0];
+    $(project_text).attr('style', 'visibility: visible; opacity: 1;');
+    $(img).attr('style', 'opacity: 0.2');
+  })
+
+
 })
